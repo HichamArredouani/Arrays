a47f36b94d384bad6ed797ceddb0c15d1db9b337
Hicham Arredouani <Hichamarredouani@hotmail.com>
2025-05-09 13:31:26 +0200

    Reverse

---
+++ src/Reverse.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Reverse {
+    public static void main (String[] args) {
+        int [] numbers = {1,2,3,4,5,6};
+
+        for (int i = numbers.length -1; i >= 0; i--) {
+            System.out.println(numbers[i] + " ");
+        }
+
+    }
+}
